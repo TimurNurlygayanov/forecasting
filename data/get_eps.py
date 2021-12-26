f091001e4fb7f9a902b1eb28526bc3d04db1914c
Timur Nurlygayanov <v.timur.nurlygayanov@reddit.com>
2021-12-26 21:05:37 +0300

    Updated script

--- data/get_eps.py
+++ data/get_eps.py
@@ -5,23 +5,72 @@ from finvizfinance.quote import finvizfinance
 with open('data/revolut_tickers.txt', 'r', encoding='utf-8') as tickers_file:
     TICKERS = tickers_file.readlines()
 
+
 TICKERS = [t.strip() for t in TICKERS if t]
 random.shuffle(TICKERS)
-TICKERS = TICKERS[:10]
+TICKERS = TICKERS
+
+TOP_TABLE = []
 
 for ticker in TICKERS:
-    stock = finvizfinance(ticker)
+    print(f'Reading data for {ticker}... ')
 
-    data = stock.TickerFundament()
+    try:
+        stock = finvizfinance(ticker)
+        data = stock.TickerFundament()
+    except:
+        continue
 
     if data['EPS (ttm)'] > '-':
-        parameter = 100 * float(data['EPS (ttm)']) / float(data['Price'])
+        price = float(data['Price'])
+        earning_per_dollar = 100 * float(data['EPS (ttm)']) / price
         volume = int(data['Volume'].replace(',', ''))
-        dept = float(data["Debt/Eq"].replace('-', '0'))
+        dept = float(data['Debt/Eq'].replace('-', '0'))
+        roe = float(data['ROE'].replace('-', '0').replace('%', ''))
+        roi = float(data['ROI'].replace('-', '0').replace('%', ''))
+        target_price = float(data['Target Price'].replace('-', '0'))
 
-        if dept < 2 and volume > 200000:
-            print(f'{ticker} {parameter:.1f}%/year {volume}')
-            print(f'Debt/Eq: {dept}, ROE: {data["ROE"]}, ROI: {data["ROI"]}')
-            print('- ' * 10)
-    # print(data)
-    # exit(1)
+        past_eps = -1
+        if data['EPS past 5Y'] != '-':
+            past_eps = float(data['EPS past 5Y'].replace('%', ''))
+
+        future_eps = -1
+        if data['EPS next 5Y'] != '-':
+            future_eps = float(data['EPS next 5Y'].replace('%', ''))
+
+        insiders_own = 0
+        if data['Insider Own'] != '-':
+            insiders_own = float(data['Insider Own'].replace('%', ''))
+
+        if dept < 5 and earning_per_dollar > 2:  #  and volume > 300000:
+            if roe > 0 and roi > 0 and past_eps > 0 and future_eps > 0:
+                if price * 1.2 < target_price:
+                    print(ticker, earning_per_dollar, past_eps)
+
+                    TOP_TABLE.append({
+                        'ticker': ticker,
+                        '$_per_dollar': round(earning_per_dollar, 1),
+                        'roe': roe,
+                        'roi': roi,
+                        'price': round(price, 2),
+                        'past_eps': past_eps,
+                        'insiders': insiders_own
+                    })
+
+TOP_TABLE = sorted(TOP_TABLE,
+                   key=lambda x: x['$_per_dollar'],
+                   reverse=True)
+for i, res in enumerate(TOP_TABLE):
+    TOP_TABLE[i]['top1'] = i
+
+TOP_TABLE = sorted(TOP_TABLE,
+                   key=lambda x: x['roi'],
+                   reverse=True)
+for i, res in enumerate(TOP_TABLE):
+    TOP_TABLE[i]['top2'] = i
+
+TOP_TABLE = sorted(TOP_TABLE,
+                   key=lambda x: x['top1'] + x['top2'])
+
+for res in TOP_TABLE:
+    print(res)
