bfa20f45ee95c034cf2a0e789f3b2d77c41bcbb1
Timur Nurlygaianov <timur.nurlygaianov@reddit.com>
2022-12-28 03:50:42 +0100

    Added script to trade on hourly timeframe

---
+++ revo/searcher_daily.py
@@ -0,0 +1,98 @@
+# strategy from https://www.youtube.com/watch?v=o_SUdccjuC4
+#
+import numpy as np
+import pandas as pd
+import pandas_ta  # for TA magic
+import vectorbt as vbt
+from plotly.subplots import make_subplots
+
+
+RSI_PERIOD = 14
+BBANDS_PERIOD = 34
+RSI_THRESHOLD = 33
+TAKE_PROFIT_THRESHOLD = 1.50  # 30 % of price increase
+STOP_LOSSES_THRESHOLD = 0.80
+MAX_LENGTH_FOR_BET = 20
+
+
+def draw(df):
+    # graph = go.Figure()
+    graph = make_subplots(rows=1, cols=1)
+    graph.update_layout(title=ticker, xaxis_rangeslider_visible=False)
+
+    graph.add_candlestick(open=df['Open'], high=df['High'], low=df['Low'], close=df['Close'])
+
+    graph.add_scatter(y=df['SMA50'], mode='lines', name='SMA50',
+                      line={'color': 'magenta'})
+    graph.add_scatter(y=df['SMA200'], mode='lines', name='SMA200',
+                      line={'color': 'blue', 'width': 3})
+
+    graph.add_scatter(y=df['EMA10'], mode='lines', name='EMA10',
+                      line={'color': '#34ff55'})
+    graph.add_scatter(y=df['EMA20'], mode='lines', name='EMA20',
+                      line={'color': '#ff4455'})
+
+    buy_signals = [i for i, (index, row) in enumerate(df.iterrows()) if row['buy_signals'] == True]
+    graph.add_scatter(x=buy_signals, y=df['Close'].values[buy_signals], name='Buy Signal', mode='markers',
+                      marker_symbol='triangle-up', marker_color='#00FE35', marker_size=15, row=1, col=1)
+
+    sell_signals = [i for i, (index, row) in enumerate(df.iterrows()) if row['sell_signals'] == True]
+    graph.add_scatter(x=sell_signals, y=df['Close'].values[sell_signals], name='Sell Signal', mode='markers',
+                      marker_symbol='triangle-down', marker_color='#D62728', marker_size=15, row=1, col=1)
+
+    graph.show()
+
+
+def run_backtest(ticker='AAPL', period='50d'):
+    buy_signals = {}
+    sell_signals = {}
+    last_buy_position = 0
+
+    df = pd.DataFrame()
+    df = df.ta.ticker(ticker, period=period, interval="1h", auto_adjust=True)
+
+    df.ta.sma(length=200, append=True, col_names=('SMA200',))
+    df.ta.sma(length=50, append=True, col_names=('SMA50',))
+
+    df.ta.ema(length=7, append=True, col_names=('EMA10',))
+    df.ta.ema(length=7*5, append=True, col_names=('EMA20',))
+
+    df = df[200:].copy()
+
+    for i, (index, row) in enumerate(df.iterrows()):
+        buy_signals[i] = False
+        sell_signals[i] = False
+
+        if last_buy_position == 0 and i > 0:
+            if row['SMA50'] > row['SMA200'] and df['SMA50'].values[i-1] < df['SMA200'].values[i-1]:
+                buy_signals[i] = True
+                last_buy_position = i
+
+        if i > last_buy_position > 0:
+            # Sell as soon as we got total desired profit:
+            if row['Close'] > TAKE_PROFIT_THRESHOLD * df['Close'].values[last_buy_position]:
+                sell_signals[i] = True
+                last_buy_position = 0
+
+            if row['SMA50'] < row['SMA200']:
+                sell_signals[i] = True
+                last_buy_position = 0
+
+    df['buy_signals'] = buy_signals.values()
+    df['sell_signals'] = sell_signals.values()
+
+    pf = vbt.Portfolio.from_signals(df.Close, entries=df['buy_signals'], exits=df['sell_signals'], freq='D',
+                                    init_cash=10_000, fees=0.0025, slippage=0.0025)
+
+    results = pf.stats()
+
+    draw(df)
+
+    print(f'\n\n----\n {ticker}')
+    print(results[['Start Value', 'End Value', 'Total Return [%]', 'Total Trades', 'Win Rate [%]']])
+
+
+if __name__ == '__main__':
+
+    for ticker in ['MSFT']:
+        run_backtest(ticker)  # 1 year is 250 days
--- rl/rl_daily/env.py
+++ rl/rl_daily/env.py
@@ -14,10 +14,13 @@ from os import path
 from plotly.subplots import make_subplots
 
 
+# Simplify rewards
+# Train with one share to win most of the time first
+# Validate & improve input data
+
 INITIAL_DEPOSIT = 3000    # The agent starts with $3000 deposit
 ORDER_FEES = 1            # $1 for execution of each order
-TICKERS = ['ALB', 'TSLA', 'HD', 'NEE', 'MSFT']
-TICKERS_TO_VERIFY = ['A', 'AAPL', 'NVDA']
+STATE_DATA = 5  # 7*5
 
 
 class ProfitEnv(gym.Env):
@@ -33,8 +36,11 @@ class ProfitEnv(gym.Env):
     mode = ''
     sell_signals = None
     buy_signals = None
+    sum_profit = 0
+    tickers = None
+    initial_tickers = []
 
-    _max_episode_steps = 100
+    _max_episode_steps = 5000  # get profit in 10 days
 
     def __init__(self, tickers_list: list = None, mode='learn'):
         super(ProfitEnv, self).__init__()
@@ -44,11 +50,13 @@ class ProfitEnv(gym.Env):
         self.action_space = spaces.Discrete(3)
 
         # Current price: Low, High, Open, Close
-        # EMA3, EMA7, EMA20, SMA50, SMA200, Bollinger_low, Bollinger_up, Volume, RSI, SuperTrend, MACD, MACD_signal,
-        # ATR, current profit,
-        self.observation_space = spaces.Box(low=-100, high=100, shape=(5, 18), dtype=np.float32)
+        # EMA3, EMA7, EMA20, EMA70, EMA200, SMA50, SMA200, SMA500, SMA2000,
+        # Bollinger_low, Bollinger_up, RSI, SuperTrend,
+        # MACD, MACD_signal, ATR, current profit,
+        self.observation_space = spaces.Box(low=-10, high=10, shape=(STATE_DATA, 28), dtype=np.float32)
 
         self.tickers = tickers_list.copy()
+        self.initial_tickers = tickers_list.copy()
 
         self.ticker = self.tickers.pop()
         self.get_data(self.ticker)
@@ -60,12 +68,25 @@ class ProfitEnv(gym.Env):
             self.df.ta.ema(length=3, append=True, col_names=('EMA3',))
             self.df.ta.ema(length=7, append=True, col_names=('EMA7',))
             self.df.ta.ema(length=20, append=True, col_names=('EMA20',))
+            self.df.ta.ema(length=35, append=True, col_names=('EMA35',))
+
+            self.df.ta.ema(length=7*8, append=True, col_names=('EMA70',))
+            self.df.ta.ema(length=20*8, append=True, col_names=('EMA200',))
 
             self.df.ta.sma(length=50, append=True, col_names=('SMA50',))
             self.df.ta.sma(length=200, append=True, col_names=('SMA200',))
 
+            self.df.ta.sma(length=50*8, append=True, col_names=('SMA500',))
+            self.df.ta.sma(length=200*8, append=True, col_names=('SMA2000',))
+
             self.df.ta.rsi(append=True, col_names=('RSI',))
+            self.df['RSI'] /= 100
+
+            self.df.ta.rsi(length=8*14, append=True, col_names=('RSI_LONG',))
+            self.df['RSI_LONG'] /= 100
+
             self.df.ta.atr(append=True, col_names=('ATR',))
+            self.df['ATR'] /= 100
 
             self.df.ta.bbands(length=34, col_names=('L', 'M', 'U', 'B', 'P'), append=True)
 
@@ -73,31 +94,34 @@ class ProfitEnv(gym.Env):
 
             self.df.ta.supertrend(append=True, length=10, multiplier=4.0,
                                   col_names=('S_trend', 'S_trend_d', 'S_trend_l', 'S_trend_s',))
-        except:
-            print(f'WRONG DATA {self.ticker}')
+        except Exception as e:
+            print(f'WRONG DATA {self.ticker} {e}')
             exit(1)
 
-        self.df = self.df[200:].copy()
+        self.df = self.df[200 * 8:].copy()
 
     def get_state(self):
         states = []
 
-        for i in range(self.current_index-5, self.current_index):
+        for i in range(self.current_index - STATE_DATA, self.current_index):
             state = []
 
             scaled_parameters = [
-                'Low', 'High', 'Open', 'Close', 'EMA3', 'EMA7', 'EMA20', 'SMA50', 'SMA200', 'L', 'U'
+                'Low', 'High', 'Open', 'Close', 'EMA3', 'EMA7', 'EMA20', 'EMA70', 'EMA200',
+                'SMA50', 'SMA200', 'SMA500', 'SMA2000', 'L', 'U'
             ]
 
             for parameter in scaled_parameters:
-                state.append(self.df[parameter].values[i] / self.df['SMA200'].values[i])
+                state.append(self.df[parameter].values[i] / self.df['SMA200'].values[self.current_index] - 1)
 
+            """
             if i == 0:
                 state.append(1)
             else:
                 state.append(self.df['Volume'].values[i] / max(self.df['Volume'].values[:i]))
+            """
 
-            parameters = ['RSI', 'S_trend_d', 'MACD', 'MACD_signal', 'ATR']
+            parameters = ['RSI', 'RSI_LONG', 'S_trend_d', 'MACD', 'MACD_signal', 'ATR']
             for parameter in parameters:
                 state.append(self.df[parameter].values[i])
 
@@ -105,8 +129,37 @@ class ProfitEnv(gym.Env):
             profit = 0
             if self.last_bought_price > 0:
                 profit = self.df['Close'].values[i] / self.last_bought_price - 1
-
             state.append(profit)
+
+            # Check flags:
+            ema20_flag = -1
+            if self.df['EMA20'].values[i] > self.df['SMA50'].values[i]:
+                ema20_flag = 1
+
+            sma50_flag = -1
+            if self.df['SMA50'].values[i] > self.df['SMA200'].values[i]:
+                sma50_flag = 1
+
+            sma500_flag = -1
+            if self.df['SMA500'].values[i] > self.df['SMA2000'].values[i]:
+                sma500_flag = 1
+
+            state.append(ema20_flag)
+            state.append(sma50_flag)
+            state.append(sma500_flag)
+
+            # Our own moving average crossover for daily trades
+            diff = self.df['EMA7'].values[i] / self.df['EMA35'].values[i] - 1
+            state.append(diff)
+
+            diff = self.df['EMA35'].values[i] / self.df['SMA200'].values[i] - 1
+            state.append(diff)
+
+            # Check Volume diff
+            volume_increases = 0
+            if i > 0 and self.df['Volume'].values[i] > self.df['Volume'].values[i-1]:
+                volume_increases = 1
+            state.append(volume_increases)
 
             states.append(state)
 
@@ -117,8 +170,32 @@ class ProfitEnv(gym.Env):
 
         reward = 0
 
-        if self.last_bought_position == 0:
-            reward = -0.005  # deduct 0.5 % for each date when we didn't have any shares
+        """
+        if action == 0:
+            # Punish for not buying shares:
+            if self.last_bought_position == 0:
+                reward = -0.02  # deduct 2 % for each date when we didn't have any shares
+
+            # Punish for holding without the profit:
+            current_price = self.df['Close'].values[self.current_index]
+
+            if 0 < current_price < self.last_bought_price:
+                reward = -1
+            elif self.last_bought_position > 0:
+                if current_price < 0.9 * max(self.df['Close'].values[self.last_bought_position:self.current_index]):
+                    reward = -0.01
+
+            if 0 < self.last_bought_price < current_price:  # Reward for holding with profit:
+                reward = 0.1
+
+                if current_price > 0.95 * max(self.df['Close'].values[self.last_bought_position:self.current_index]):
+                    reward = 0.2
+        """
+
+        if action == 0:
+            if self.last_bought_price > 0:
+                current_price = self.df['Close'].values[self.current_index]
+                reward = current_price / self.last_bought_price - 1
 
         if action == 1:
             if self.last_bought_position == 0:
@@ -126,7 +203,7 @@ class ProfitEnv(gym.Env):
 
                 bet_size = min(self.budget, 20000)  # limit size of every deal
 
-                number_of_shares = bet_size // current_price
+                number_of_shares = 10  # bet_size // current_price
 
                 if number_of_shares > 0:
                     self.budget -= number_of_shares * current_price
@@ -137,6 +214,8 @@ class ProfitEnv(gym.Env):
                     self.total_deals += 1
 
                     self.buy_signals[self.current_index] = True
+
+                    reward = -2  # reward for making a bet - make it profitable only if we got +2% from bet
 
         if action == 2:
             if self.last_bought_position > 0:  # sell only if we have something to sell
@@ -156,22 +235,28 @@ class ProfitEnv(gym.Env):
                 with open(f"rl/deals_{self.mode}.txt", 'a+') as myfile:
                     myfile.write(f'sell {self.ticker} profit: '
                                  f'{100 * (current_price / self.last_bought_price - 1):.1f}% '
-                                 f'total: {self.budget:.0f}\n')
+                                 f'total: ${self.budget:.0f}\n')
 
                 # positive reward in case we got profit, negative reward otherwise
-                reward = current_price / self.last_bought_price - 1
+                profit_percent = current_price / self.last_bought_price - 1
+                reward = int(profit_percent * 100)
+
+                # if we got large profit or loss, multiply the reward/punishment to highlight the impact
+                if abs(reward) > 10:
+                    reward = int(reward * 1.2)
+
+                """
+                if reward < 0:
+                    reward = int(reward * 2)
+                """
+
+                self.sum_profit += reward
+
+                self.sell_signals[self.current_index] = True
 
                 self.number_of_shares_bought = 0
                 self.last_bought_price = 0
                 self.last_bought_position = 0
-
-                self.sell_signals[self.current_index] = True
-
-        if reward < 0:
-            reward *= 5  # punish the agent for loosing money
-
-        if reward > 1.03:
-            reward *= 3  # reward agent for large profit
 
         self.current_index += 1
         done = bool(self.current_index >= len(self.df['Close']) - 1)
@@ -180,25 +265,32 @@ class ProfitEnv(gym.Env):
             done = False
 
             ticker = self.tickers.pop()
+            # print(f'new ticker {ticker} left: {len(self.tickers)}')
 
             self.get_data(ticker)
             self.prepare_data()
             self.reset(cleanup_budget=False)
+        elif not self.tickers and self.mode != 'test':
+            self.tickers = self.initial_tickers.copy()
+
+        budget = self.budget + self.last_bought_price * self.number_of_shares_bought
+        if budget < 500:
+            self.budget = 3000
 
         return self.get_state(), reward, done, {}
 
     def get_data(self, ticker):
-        file_name = f'rl/data/{ticker}.xlsx'
+        file_name = f'rl/data/daily_{ticker}.xlsx'
         self.ticker = ticker
 
         if path.exists(file_name):
             self.df = pd.read_excel(file_name, index_col=0)  # , header=[0, 1]
         else:
             self.df = pd.DataFrame()
-            self.df = self.df.ta.ticker(ticker, period='1000d')
+            self.df = self.df.ta.ticker(ticker, period='700d', interval='1h')
             self.prepare_data()
 
-            self.df.index = self.df.index.strftime('%m/%d/%Y')
+            self.df.index = self.df.index.strftime('%m/%d/%Y %H:%M')
             self.df.to_excel(file_name, index=True, header=True)
 
     def reset(self, cleanup_budget=True):
@@ -208,15 +300,18 @@ class ProfitEnv(gym.Env):
             if self.last_bought_position > 0:
                 self.budget += self.last_bought_price * self.number_of_shares_bought
 
-        self.current_index = 5  # we start from the Day #5 in the data set to have a history
+        self.current_index = STATE_DATA  # we start from the Day #5 in the data set to have a history
         self.last_bought_position = 0
         self.last_bought_price = 0
         self.number_of_shares_bought = 0
+        self.sum_profit = 0
 
         self.buy_signals = {i: False for i in range(len(self.df))}
         self.sell_signals = {i: False for i in range(len(self.df))}
 
-        return self.get_state()
+        state = self.get_state()
+        # print(state)
+        return state
 
     def draw(self):
         df = self.df
@@ -242,7 +337,7 @@ class ProfitEnv(gym.Env):
         graph.show()
 
     def render(self, mode='human', close=False):
-        if self.total_deals % 10 == 0:
+        if self.total_deals > 0 and self.total_deals % 100 == 0:
             total_budget = self.budget
             if self.last_bought_position > 0:
                 total_budget += self.last_bought_price * self.number_of_shares_bought
@@ -259,20 +354,28 @@ class ProfitEnv(gym.Env):
             self.df['sell_signals'] = self.sell_signals.values()
             self.draw()
 
+        """
+        if self.mode == 'test':
+            print(self.current_index)
+            print(self.get_state())
+        """
+
 
 with open('smp500.txt', 'r') as f:
     TICKERS = f.readlines()
 
 TICKERS = [t.replace('\n', '') for t in TICKERS if '^' not in t and '/' not in t and '.' not in t]
 TICKERS.remove('CEG')
+TICKERS.remove('ELV')
 
-non_vectorized_env = ProfitEnv(TICKERS[:200].copy())
+non_vectorized_env = ProfitEnv(TICKERS[:1].copy())
 env = DummyVecEnv([lambda: non_vectorized_env])
 
 model = PPO('MlpPolicy', env, verbose=True, tensorboard_log='logs')  # learning_rate=0.0001
-model.learn(total_timesteps=200000)
+model.learn(total_timesteps=500000)
 
-non_vectorized_env = ProfitEnv(TICKERS[200:205].copy(), mode='test')
+print(TICKERS[:5])
+non_vectorized_env = ProfitEnv(TICKERS[:1].copy(), mode='test')
 env = DummyVecEnv([lambda: non_vectorized_env])
 
 evaluate_policy(model, env, n_eval_episodes=1, render=True)
